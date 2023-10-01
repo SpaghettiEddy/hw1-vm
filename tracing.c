d0abaf8c7fd31df30678627699dff3694ca5b9d1
SantiagoMS03 <santiagoms2003@gmail.com>
2023-10-01 16:10:40 -0400

    tracing

---
+++ tracing.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "vm.h"
+#include "machine_types.h"
+#include "bof.h"
+#include "instruction.h"
+#include "regname.h"
+#include "utilities.h"
+#include "file_location.h"
+#include "reg.h"
+#include "jump.h"
+#include "immed.h"
+#include "sys.h"
+#include "scanbof.h"
+
+void print_pc() {
+    printf("\t  PC: %u\n", regi.pc)l
+}
+void print_registers() {
+    for (int i = 0; i < 32; i++) {
+        printf("GPR[%s]: %u\t%c", regname_get(i), regi.GPR[i], ((i % 6 == 5) || (i == 31)) * '\n');
+    }
+}
+void print_pointers() {
+    // Need to understand first
+}
+
+void print_stack_address() {
+    // Missing index of instruction. PC - 1? PC - 4? PC? PC + 1? PC + 4?
+    printf("==> addr:\t%u %s", regi.pc, instruction_assembly_form(mem.instrs[regi.pc]))
+}
+
+void print_state() {
+    print_pc();
+    print_registers();
+    print_pointers();
+    print_stack_address();
+}
---
+++ tracing.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "vm.h"
+#include "machine_types.h"
+#include "bof.h"
+#include "instruction.h"
+#include "regname.h"
+#include "utilities.h"
+#include "file_location.h"
+#include "reg.h"
+#include "jump.h"
+#include "immed.h"
+#include "sys.h"
+#include "scanbof.h"
+
+extern void print_registers();
+extern void print_pc();
+extern void print_pointers();
+extern void print_stack_address();
+extern void print_state();
