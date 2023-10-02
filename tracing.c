7edcc7f4625e339a30ca2c9813329d49b3383d16
Confused404 <116277026+Confused404@users.noreply.github.com>
2023-10-01 21:58:28 -0400

    only missing one thing

--- tracing.c
+++ tracing.c
@@ -21,7 +21,10 @@ void print_pc() {
 void print_registers() {
     printf("GPR[%s ]: 0   	", regname_get(0));
     for (int i = 1; i < 32; i++) {
-        printf("GPR[%s]: %u   	", regname_get(i), regi.GPR[i]);
+        if(regi.GPR[i-1] > 1000 || regi.GPR[i] > 1000)
+            printf("GPR[%s]: %u  ", regname_get(i), regi.GPR[i]);
+        else
+            printf("GPR[%s]: %u   	", regname_get(i), regi.GPR[i]);
         if ((i % 6 == 5) || i == 31) printf("\n");
     }
 }
